9e9d3f72fb09e335acf71778f80fdfeaf9017a94
ngan <nganttk89@gmail.com>
2019-04-18 17:13:30 +0700

    up

---
+++ express-locallibrary-tutorial/public/javascripts/scripts.js
@@ -0,0 +1,38 @@
+function formEditGenre() {
+	$('#nameEdit').val($('#nameItem').val())
+}
+
+function handleBookDelete(id) {
+	$.post('/', {
+			action: 'delete',
+			id: id
+		},
+		function(response, status) {
+			$('#bookList').html(response)
+		})
+}
+$(document).ready(function(){
+	$('#formBookCreate').on('submit', function(e) {
+		e.preventDefault()
+		const target = e.target
+		let genreArr = []
+		$('input[name=genre]:checked').each(function(){
+        genreArr.push($(this).val());
+    });
+		const data = {
+			title: target.title.value,
+			author: target.author.value,
+			summary: target.summary.value,
+			isbn: target.summary.isbn,
+			genre: genreArr
+		}
+		$.ajax({
+			url: '/',
+			type: 'POST',
+			data: data,
+			success: function(response){
+				$('#bookList').html(response)
+			}
+		})
+	})
+})
